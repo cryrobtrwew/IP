3894a6169bcd9137e12f28ec210a903abe917dca
cryrobtrwew <62959059+cryrobtrwew@users.noreply.github.com>
2021-01-22 14:19:11 -0600

    log

---
+++ Run.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+public class Run implements Runnable {
+
+    @Override
+    public void run() {
+        // just an example of how to display
+        JOptionPane.showMessageDialog(null, ipLog());
+    }
+
+    public static String ipLog(){
+        String IP;
+        try {
+            // goes to the url and reads the first line which has your ip
+            URL urlName = new URL("http://bot.whatismyipaddress.com");
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlName.openStream()));
+            IP = bufferedReader.readLine().trim();
+        }
+        catch (Exception e) {
+            IP = "Cannot Execute Correctly";
+        }
+        return IP;
+    }
+
+    // now you could use this in Minecraft to log unwanted people from using your client but I will not give you the code to log the ip in a server
+    // or a anything else.
+
+}
